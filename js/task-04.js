8a58e0b29f9570ab87cc6bbed76d71c621d56a04
Dmytro Ruduik <119795560+Ruduke@users.noreply.github.com>
2023-04-25 20:06:46 +0200

    Update task-04.js
    Added a standard solution of the task, implying counter initialization

--- js/task-04.js
+++ js/task-04.js
@@ -1,3 +1,5 @@
+// NO COUNTER INITIALIZATION SOLUTION
+
 const counterValue = document.getElementById("value");
 const decrementButton = document.querySelector('[data-action="decrement"]');
 const incrementButton = document.querySelector('[data-action="increment"]');
@@ -9,3 +11,21 @@ decrementButton.addEventListener("click", () => {
 incrementButton.addEventListener("click", () => {
   counterValue.textContent = Number(counterValue.textContent) + 1;
 });
+
+// STANDARD SOLUTION
+
+// const spanValue = document.querySelector("#value");
+// const decrementBtn = document.querySelector('[data-action="decrement"]');
+// const incrementBtn = document.querySelector('[data-action="increment"]');
+
+// let counterValue = 0;
+
+// decrementBtn.addEventListener("click", () => {
+//   counterValue -= 1;
+//   spanValue.textContent = counterValue;
+// });
+
+// incrementBtn.addEventListener("click", () => {
+//   counterValue += 1;
+//   spanValue.textContent = counterValue;
+// });
