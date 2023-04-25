d30084819496f46a66246776cda9739ff1d5e6c1
Dmytro Ruduik <119795560+Ruduke@users.noreply.github.com>
2023-04-25 19:21:54 +0200

    Update task-05.js
    Replaced Ternary Operator with Logical OR on Line 5

--- js/task-05.js
+++ js/task-05.js
@@ -2,6 +2,6 @@ const nameInput = document.getElementById("name-input");
 const nameOutput = document.getElementById("name-output");
 
 nameInput.addEventListener("input", () => {
-  const name = nameInput.value.trim() ? nameInput.value : "Anonymous";
+  const name = nameInput.value.trim() || "Anonymous"; // REPLACED [const name = nameInput.value.trim() ? nameInput.value : "Anonymous";] WITH THE LOGICAL OR
   nameOutput.textContent = name;
 });
